0da1cc21c676848dcb4554b1d5acbc581a417ab4
Johnson Jayaraj <tempnjayaraj@gmail.com>
2022-03-11 01:00:01 +0530

    Initial Commit

---
+++ src/main/java/com/gqms/archimedis/globalq_optiqs360_sn/App.java
@@ -0,0 +1,13 @@
+package com.gqms.archimedis.globalq_optiqs360_sn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/gqms/archimedis/globalq_optiqs360_sn/AppTest.java
@@ -0,0 +1,20 @@
+package com.gqms.archimedis.globalq_optiqs360_sn;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
